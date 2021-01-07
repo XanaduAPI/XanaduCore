5fc9a509c8bcb8f0eb58bca3e24bbaa05ea428eb
怯花颜 <45027459+XanaduAPI@users.noreply.github.com>
2021-01-07 00:58:15 -0600

    The header file contains modifications

--- include/XanaduCore/XanaduCore.h
+++ include/XanaduCore/XanaduCore.h
@@ -1,6 +1,8 @@
 ﻿#ifndef			_XANADU_CORE_H_
 #define			_XANADU_CORE_H_
 
+#include <XanaduCore/XanaduCoreAllocator.h>
 #include <XanaduCore/XanaduCoreByteArray.h>
+#include <XanaduCore/XanaduCoreString.h>
 
 #endif//_XANADU_CORE_H_
