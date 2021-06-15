7e5d7253fb49319723241047f92faa23fb6ebc57
怯花颜 <45027459+XanaduAPI@users.noreply.github.com>
2021-06-15 18:06:30 +0800

    Fix Windows process pipeline problem

--- source/XShell.cpp
+++ source/XShell.cpp
@@ -32,6 +32,113 @@ int32S XShell::run(const XString& _Shell, std::function<bool(const XString& _Out
 {
 	XANADU_CHECK_RETURN(_Shell.exist(), -1);
 
+#if defined(_XANADU_SYSTEM_WINDOWS)
+	SECURITY_ATTRIBUTES	sa;
+	HANDLE			vRHandle = nullptr;
+	HANDLE			vWHandle = nullptr;
+	sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+	sa.lpSecurityDescriptor = nullptr;
+	sa.bInheritHandle = TRUE;
+
+	// 创建匿名管道
+	if (::CreatePipe(&vRHandle, &vWHandle, &sa, 0))
+	{
+		STARTUPINFOW		si;
+		PROCESS_INFORMATION	pi;
+		Xanadu::memset(&si, 0, sizeof(STARTUPINFOW));
+		si.cb = sizeof(STARTUPINFO);
+		::GetStartupInfoW(&si);
+		si.hStdError = vWHandle;
+		si.hStdOutput = vWHandle;
+		si.wShowWindow = SW_HIDE;
+		si.dwFlags = STARTF_USESHOWWINDOW | STARTF_USESTDHANDLES;
+
+		wchar_t 	vApplication[XANADU_PATH] = {0};
+		auto		vCommandLine = XANADU_NEW wchar_t[static_cast<size_t>(_Shell.size()) + 100];
+		if(vCommandLine)
+		{
+			Xanadu::wmemset(vCommandLine, 0, static_cast<size_t>(_Shell.size()) + 100);
+			auto		vPos = XString::npos;
+			if(_Shell[0] == L'\"')
+			{
+				vPos = _Shell.find(L'\"', 1U);
+				if(vPos != XString::npos)
+				{
+					vPos += 1;
+				}
+			}
+			else
+			{
+				vPos = _Shell.find(L' ');
+			}
+			if(vPos == XString::npos)
+			{
+				vPos = 0;
+				Xanadu::wmemcpy(vApplication, _Shell.data(), vPos);
+			}
+			else
+			{
+				Xanadu::wmemcpy(vApplication, _Shell.data(), vPos);
+				vPos += 1;
+			}
+			Xanadu::wmemcpy(vCommandLine, _Shell.data() + vPos, static_cast<size_t>(_Shell.size() - vPos));
+
+			// 创建子进程
+			if (::CreateProcessW(vApplication, vCommandLine, nullptr, nullptr, TRUE, NULL, nullptr, nullptr, &si, &pi))
+			{
+				::CloseHandle(vWHandle);
+
+				auto		vBuffer = XANADU_NEW char[XANADU_SIZE_KB];
+				auto		vBytes = XByteArray();
+				auto		vSync = static_cast<DWORD>(0);
+				for(; vBuffer;)
+				{
+					Xanadu::memset(vBuffer, 0, XANADU_SIZE_KB);
+					auto		vSuccess = (::ReadFile(vRHandle, vBuffer, XANADU_SIZE_KB, &vSync, nullptr) == TRUE);
+					if(vSync > 0)
+					{
+						vBytes += XByteArray(vBuffer, Xanadu::strlen(vBuffer));
+					}
+					auto		vFind = vBytes.find('\n');
+					if(vFind != XByteArray::npos)
+					{
+						if(_Lambda)
+						{
+							_Lambda(XString::fromBytes(vBytes.left(vFind)));
+						}
+						vBytes.remove(static_cast<XByteArray::size_type>(0U), vFind + 1);
+					}
+					if(!vSuccess)
+					{
+						if(ERROR_BROKEN_PIPE == ::GetLastError())
+						{
+							break;
+						}
+					}
+				}
+				if(_Lambda)
+				{
+					_Lambda(XString::fromBytes(vBytes));
+				}
+				if(vBuffer)
+				{
+					XANADU_DELETE_ARR(vBuffer);
+				}
+
+				::CloseHandle(vRHandle);
+
+				// 等待进程结束
+				::WaitForSingleObject(pi.hProcess, INFINITE);
+				auto		vExitCode = static_cast<DWORD>(STILL_ACTIVE);
+				::GetExitCodeProcess(pi.hProcess, &vExitCode);
+				// ::CloseHandle(pi.hProcess);
+				XANADU_DELETE_ARR(vCommandLine);
+				return vExitCode;
+			}
+			XANADU_DELETE_ARR(vCommandLine);
+		}
+	}
+#else
 	auto		vHandle = Xanadu::popen(_Shell.toNString().data(), "r");
 	if(vHandle)
 	{
@@ -70,6 +177,7 @@ int32S XShell::run(const XString& _Shell, std::function<bool(const XString& _Out
 		}
 		return Xanadu::pclose(vHandle);
 	}
+#endif
 	return -1;
 }
 
